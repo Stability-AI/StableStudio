8991c4ee284a095e97ac2f5c13435a0f92570424
Bryce <github20210803@accounts.brycedrennan.com>
2023-05-21 23:25:18 -0700

    feature: add command to start with imaginairy plugin

--- packages/stablestudio-ui/src/Environment/index.tsx
+++ packages/stablestudio-ui/src/Environment/index.tsx
@@ -8,6 +8,7 @@ declare global {
   interface ImportMetaEnv {
     readonly VITE_GIT_HASH: string;
     readonly VITE_USE_EXAMPLE_PLUGIN: string;
+    readonly VITE_USE_IMAGINAIRY_PLUGIN: string;
   }
 }
 
@@ -20,6 +21,7 @@ export namespace Environment {
   const variables = {
     VITE_GIT_HASH: import.meta.env.VITE_GIT_HASH,
     VITE_USE_EXAMPLE_PLUGIN: import.meta.env.VITE_USE_EXAMPLE_PLUGIN ?? "false",
+    VITE_USE_IMAGINAIRY_PLUGIN: import.meta.env.VITE_USE_IMAGINAIRY_PLUGIN ?? "false",
   } as const;
 
   export function get(name: VariableName): string {
