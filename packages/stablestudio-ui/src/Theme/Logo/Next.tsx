acdcbbcc34159da025e1ff864d66906bbb3860fa
Harry Wang <harryjwang@gmail.com>
2023-05-27 12:55:08 -0400

    minor fix

--- packages/stablestudio-ui/src/Theme/Logo/Next.tsx
+++ packages/stablestudio-ui/src/Theme/Logo/Next.tsx
@@ -7,7 +7,7 @@ export function Next() {
       <div className="flex flex-col">
         <span className="text-lg font-medium">StableStudio</span>
         <span className="-mt-1 text-xs font-light">
-          <span className="text-black/75 dark:text-white/75">by </span>
+        <span className="text-black/75 dark:text-white/75">by </span>
           stability.ai
         </span>
       </div>
