4ae2e1766ebda6169b6ad89a4a5f96af3687dad1
jtydhr88 <terryjia88@gmail.com>
2023-05-31 13:55:51 -0400

    webui plugin: check empty str

--- packages/stablestudio-plugin-webui/src/index.ts
+++ packages/stablestudio-plugin-webui/src/index.ts
@@ -94,7 +94,6 @@ export const createPlugin = StableStudio.createPlugin<{
     | "getStableDiffusionDefaultInput"
     | "getStableDiffusionExistingImages"
   > => {
-    webuiHostUrl = webuiHostUrl ?? "http://127.0.0.1:7861";
 
     return {
       createStableDiffusionImages: async (options) => {
@@ -244,8 +243,11 @@ export const createPlugin = StableStudio.createPlugin<{
     };
   };
 
-  const webuiHostUrl =
-    localStorage.getItem("webui-host-url") ?? "http://127.0.0.1:7861";
+  let webuiHostUrl = localStorage.getItem("webui-host-url");
+
+  if (!webuiHostUrl || webuiHostUrl === "") {
+    webuiHostUrl = "http://127.0.0.1:7861";
+  }
 
   return {
     ...webuiLoad(webuiHostUrl),
