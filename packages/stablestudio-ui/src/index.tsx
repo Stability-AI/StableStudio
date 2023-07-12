e0f787c4d74bd688d8d4e6ef37f7ebadd2a53134
kaj <40004347+KAJdev@users.noreply.github.com>
2023-07-12 10:56:31 -0800

    attempt to package comfyui

--- packages/stablestudio-ui/src/index.tsx
+++ packages/stablestudio-ui/src/index.tsx
@@ -1,8 +1,20 @@
+import { readTextFile } from "@tauri-apps/api/fs";
+import { resolveResource } from "@tauri-apps/api/path";
+// alternatively, use `window.__TAURI__.path.resolveResource`
+// alternatively, use `window.__TAURI__.fs.readTextFile`
+
 import ReactDOM from "react-dom/client";
 
 import "~/GlobalVariables";
 
 import { App } from "~/App";
+
+(async () => {
+  const resourcePath = await resolveResource(
+    "comfyui/README_VERY_IMPORTANT.txt"
+  );
+  console.log(resourcePath, await readTextFile(resourcePath));
+})();
 
 const main = async () => {
   const root = document.getElementById("app");
