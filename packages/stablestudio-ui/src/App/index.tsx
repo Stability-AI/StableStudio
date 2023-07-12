58075b422eef15bc996b70571f02013913b77270
kaj <40004347+KAJdev@users.noreply.github.com>
2023-07-12 14:36:01 -0800

    spike out setup flow logic

--- packages/stablestudio-ui/src/App/index.tsx
+++ packages/stablestudio-ui/src/App/index.tsx
@@ -1,3 +1,4 @@
+import { BaseDirectory, exists } from "@tauri-apps/api/fs";
 import { Router } from "~/Router";
 import { Shortcut } from "~/Shortcut";
 import { Theme } from "~/Theme";
@@ -9,26 +10,41 @@ import { TopBar } from "./TopBar";
 
 export function App() {
   const isMobileDevice = Theme.useIsMobileDevice();
+  const { isSetup, message, progress } = App.useSetupState();
+
   return useMemo(
     () => (
       <Providers>
-        <div className="absolute top-0 left-0 -z-50 h-screen w-screen dark:bg-zinc-800" />
-        <div className="absolute top-0 left-0 flex h-screen w-screen flex-col text-white sm:overflow-x-auto">
-          <Shortcut.Palette />
-          <TopBar />
-          <Sidebars />
-          <div className="flex min-h-0 grow overflow-auto sm:min-w-[1000px]">
-            <Sidebar position="left" />
-            <div className="shrink grow overflow-y-auto">
-              <Router />
-            </div>
-            <Sidebar position="right" />
-          </div>
-          {isMobileDevice && <BottomBar />}
+        <div className="absolute left-0 top-0 -z-50 h-screen w-screen dark:bg-zinc-800" />
+        <div className="absolute left-0 top-0 flex h-screen w-screen flex-col text-white sm:overflow-x-auto">
+          {isSetup !== App.SetupState.WeightsInstalled ? (
+            <>
+              <div className="flex flex-grow flex-col items-center justify-center gap-4">
+                <h1 className="text-6xl font-bold">Welcome to StableStudio</h1>
+                <p className="font-mono opacity-75">
+                  {message} {Math.round(progress * 100)}%
+                </p>
+              </div>
+            </>
+          ) : (
+            <>
+              <Shortcut.Palette />
+              <TopBar />
+              <Sidebars />
+              <div className="flex min-h-0 grow overflow-auto sm:min-w-[1000px]">
+                <Sidebar position="left" />
+                <div className="shrink grow overflow-y-auto">
+                  <Router />
+                </div>
+                <Sidebar position="right" />
+              </div>
+              {isMobileDevice && <BottomBar />}
+            </>
+          )}
         </div>
       </Providers>
     ),
-    [isMobileDevice]
+    [isMobileDevice, isSetup, message, progress]
   );
 }
 
@@ -39,4 +55,50 @@ export declare namespace App {
 export namespace App {
   App.Sidebar = Sidebar;
   App.TopBar = TopBar;
+
+  export enum SetupState {
+    NotStarted,
+    ComfyInstalled,
+    WeightsInstalled,
+  }
+
+  export const useSetupState = () => {
+    const [isSetup, setIsSetup] = useState<SetupState>(SetupState.NotStarted);
+    const [message, setMessage] = useState<string>("");
+    const [progress] = useState<number>(0);
+
+    const check = async () => {
+      const modelExists = await exists(
+        "comfyui/ComfyUI/models/checkpoints/sd_xl_base_0.9.safetensors",
+        {
+          dir: BaseDirectory.AppData,
+        }
+      );
+
+      if (!modelExists) {
+        const comfyExists = await exists(
+          "comfyui/ComfyUI/models/checkpoints/put_checkpoints_here",
+          {
+            dir: BaseDirectory.AppData,
+          }
+        );
+
+        if (!comfyExists) {
+          setIsSetup(SetupState.NotStarted);
+          setMessage("Installing ComfyUI...");
+        } else {
+          setIsSetup(SetupState.ComfyInstalled);
+          setMessage("Downloading Stable Diffusion...");
+        }
+      } else {
+        setIsSetup(SetupState.WeightsInstalled);
+      }
+    };
+
+    useEffect(() => {
+      check();
+    }, []);
+
+    return { isSetup, message, progress };
+  };
 }
