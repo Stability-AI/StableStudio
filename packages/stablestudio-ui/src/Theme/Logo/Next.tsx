c0b308e2ec4ad5056f606f70c9a42ebd73542ca6
Aniruddha1701 <95077620+Aniruddha1701@users.noreply.github.com>
2023-10-27 14:48:05 +0530

    Update Next.tsx

--- packages/stablestudio-ui/src/Theme/Logo/Next.tsx
+++ packages/stablestudio-ui/src/Theme/Logo/Next.tsx
@@ -5,9 +5,9 @@ export function Next() {
     <div className="flex items-center gap-1.5">
       <Logo />
       <div className="flex flex-col">
-        <span className="text-lg font-medium">StableStudio</span>
+        <span className="text-lg font-medium">Ai Image Generator</span>
         <span className="-mt-1 text-xs font-light">
-          <span className="text-white/75">by </span>stability.ai
+          <span className="text-white/75">by </span>Anirudh
         </span>
       </div>
     </div>
